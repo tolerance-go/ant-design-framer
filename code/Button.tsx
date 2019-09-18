a5f9e1adeda7480f001e4c929716f976c4e10512
bzone <yarnbCoder@gmail.com>
2019-09-18 17:30:38 +0800

    chore: update button code order

--- code/Button.tsx
+++ code/Button.tsx
@@ -1,49 +1,62 @@
-import * as React from "react"
-import { Frame, addPropertyControls, ControlType, RenderTarget } from "framer"
-import { Button as AntBtn } from "antd"
-import { isEmpty } from "lodash"
+import * as React from "react";
+import {
+  Frame,
+  addPropertyControls,
+  ControlType,
+  RenderTarget,
+  PropertyControls
+} from "framer";
+import { Button as AntBtn } from "antd";
+import { isEmpty } from "lodash";
+import { pick, keys } from "lodash";
 
-export function Button(props) {
-    const { children, label, ...rest } = props
-    return <AntBtn {...rest}>{isEmpty(children) ? label : children}</AntBtn>
-}
+const controlProperty: PropertyControls = {
+  label: { type: ControlType.String },
+  icon: { type: ControlType.String },
+  type: {
+    type: ControlType.Enum,
+    options: ["default", "primary", "danger", "link", "dashed"]
+  },
+  size: {
+    type: ControlType.SegmentedEnum,
+    options: ["default", "small", "large"]
+  },
+  loading: { type: ControlType.Boolean },
+  block: { type: ControlType.Boolean },
+  shape: {
+    type: ControlType.SegmentedEnum,
+    options: ["default", "circle", "round"]
+  },
+  disabled: { type: ControlType.Boolean },
+  ghost: { type: ControlType.Boolean },
+  href: { type: ControlType.String },
+  htmlType: { type: ControlType.String },
+  children: {
+    type: ControlType.ComponentInstance
+  },
+};
+
+export const Button = props => {
+  const { children, label, ...rest } = props;
+  return (
+    <AntBtn {...pick(rest, keys(controlProperty))}>
+      {isEmpty(children) ? label : children}
+    </AntBtn>
+  );
+};
 
 Button.defaultProps = {
-    width: 70,
-    height: 32,
-    label: "button",
-    type: "primary",
-    size: "default",
-    shape: "default",
-    loading: false,
-    block: false,
-    disabled: false,
-    ghost: false,
-    onClick: () => {},
-}
+  width: 70,
+  height: 32,
+  label: "button",
+  type: "primary",
+  size: "default",
+  shape: "default",
+  loading: false,
+  block: false,
+  disabled: false,
+  ghost: false,
+  onClick: () => {}
+};
 
-addPropertyControls(Button, {
-    label: { type: ControlType.String },
-    icon: { type: ControlType.String },
-    type: {
-        type: ControlType.Enum,
-        options: ["default", "primary", "danger", "link", "dashed"],
-    },
-    size: {
-        type: ControlType.SegmentedEnum,
-        options: ["default", "small", "large"],
-    },
-    loading: { type: ControlType.Boolean },
-    block: { type: ControlType.Boolean },
-    shape: {
-        type: ControlType.SegmentedEnum,
-        options: ["default", "circle", "round"],
-    },
-    disabled: { type: ControlType.Boolean },
-    ghost: { type: ControlType.Boolean },
-    href: { type: ControlType.String },
-    htmlType: { type: ControlType.String },
-    children: {
-        type: ControlType.ComponentInstance,
-    },
-})
+addPropertyControls(Button, controlProperty);
