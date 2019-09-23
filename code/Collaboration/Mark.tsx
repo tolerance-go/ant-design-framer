44f0e9abcdbeb4ef964b8195fd18f23b9e0589e4
bzone <yarnbCoder@gmail.com>
2019-09-23 14:22:16 +0800

    perf: change mark preview component

--- code/Collaboration/Mark.tsx
+++ code/Collaboration/Mark.tsx
@@ -14,69 +14,90 @@ import {
   Row,
   Col,
   Button,
-  Tag
+  Tag,
+  Popover,
+  Divider
 } from "antd";
 import { keys, pick, zipObject, fill, range } from "lodash";
 
 const controlProperty: PropertyControls = {
   count: { type: ControlType.Number },
-  content: { type: ControlType.String }
+  content: { type: ControlType.String },
+  visible: { type: ControlType.Boolean }
 };
 
 export const Mark = props => {
-  const { count, content, ...rest } = props;
+  const { count, content, visible: _visible, iconType, ...rest } = props;
   const [visible, setVisible] = React.useState(false);
+
+  const okType = "primary";
+  const title = content;
+  const placement = "top";
+
   return (
-    <>
-      <Badge
-        {...pick(rest, keys(controlProperty))}
-        count={count}
-        showZero
-        style={{ background: "#52c41a", cursor: "pointer" }}
-        onClick={() => {
-          setVisible(true);
+    <div>
+      <div
+        style={{
+          position: "relative",
+          opacity: getVisible() ? 1 : 0
         }}
-      ></Badge>
-      <Modal
-        closable={false}
-        visible={visible}
-        footer={
-          <Row type="flex" justify="end" align="middle">
-            <Col>
-              <Row
-                type="flex"
-                justify="space-between"
-                align="middle"
-                gutter={10}
-              >
-                <Col>
-                  <Button size="small" disabled>
+        className={`ant-popover ant-popover-placement-${placement}`}
+      >
+        <div className="ant-popover-content">
+          <div className="ant-popover-arrow"></div>
+          <div className="ant-popover-inner" role="tooltip">
+            <div>
+              <div className="ant-popover-inner-content">
+                <div style={{ marginBottom: 12 }}>{title}</div>
+                <div className="ant-popover-buttons">
+                  <Button
+                    disabled
+                    size="small"
+                  >
                     comment
                   </Button>
-                </Col>
-                <Col>
                   <Button
-                    type="primary"
                     size="small"
-                    onClick={() => setVisible(false)}
+                    type={okType}
+                    onClick={() => {
+                      setVisible(false);
+                    }}
                   >
                     fine
                   </Button>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        }
-      >
-        {content}
-      </Modal>
-    </>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Row type="flex" justify="center">
+        <Badge
+          {...pick(rest, keys(controlProperty))}
+          count={count}
+          showZero
+          style={{ background: "#52c41a", cursor: "pointer" }}
+          onClick={() => {
+            setVisible(true);
+          }}
+        ></Badge>
+      </Row>
+    </div>
   );
+
+  function getVisible() {
+    if (RenderTarget.current() === RenderTarget.preview) {
+      return visible;
+    }
+    return _visible;
+  }
 };
 
 Mark.defaultProps = {
-  width: 40,
-  height: 40
+  width: 285,
+  height: 130,
+  visible: true,
+  content: "content"
 };
 
 addPropertyControls(Mark, controlProperty);
